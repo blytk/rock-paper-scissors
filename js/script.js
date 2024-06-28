6bfd29b3739b8bede554af6a0df0f3d26d6736df
Yago <dolmayan@gmail.com>
2024-06-28 14:45:24 +0200

    Define pseudocode

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Get Computer Choice
+    // Create a new function named getComputerChoice, that randomly returns "rock", "paper", "scissors"
+        
+
+// Get Human Choice
+    // Create function getHumanChoice
+        // prompt user
+        // validate prompt is "rock", "paper", "scissors" (check caps and spaces)
+
+// Declare the players score variables
+    // Create two variables in global scope, initiliaze to 0:
+        // humanScore
+        // computerScore 
+
+// Write the logic to play a single round
+    // Create a function playRound(humanChoice, computerChoice)
+    // Console.log round winner
+    // increment humanScore or computerScore
+
+// Write the logic to play the entire game
+    // Create function playGame
+    // Move playRound function and score variables so that they're declared inside of the new playGame function
+    // Play 5 rounds by calliung playRound 5 times
